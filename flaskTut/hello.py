363faf1e5e0d9020a35769f603c276d2271a638b
Kamil <kamiladamiec95@wp.pl>
2023-01-14 10:20:45 +0100

    frontend practise

--- flaskTut/hello.py
+++ flaskTut/hello.py
@@ -1,14 +1,43 @@
 from flask import Flask, render_template
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
 
+class NamerForm(FlaskForm):
+    name = StringField("Whats your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    favourite_pizza = ["Pepperoni", "Margarita"]
+    return render_template("index.html",favourite_pizza=favourite_pizza)
 
 
 @app.route('/user/<name>')
 def user(name):
-    return f"<h1>Hello {name}</h1>"    
+    first_name = "test"
+    return render_template("user.html", user_name=name, first_name=first_name)   
+
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"),404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"),500
+
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('name.html', name=name, form=form)
